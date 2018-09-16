b66b8d112937e2523b252e3d1164512a283f5eac
parse-tech <parse.technologies@gmail.com>
2018-09-16 09:05:38 -0400

    Added changes to app.py file

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import db_connect
 import pymysql
 import board_blog_forms
 import db_actions
+import back_end
 
 app = Flask(__name__)
 app.secret_key = 'nklfAEJiku0*()&ijlkdnlkmklLKHn..4lk4'
@@ -32,9 +33,15 @@ def cmd_list():
         return redirect(url_for('cli_information'))
     return render_template('cli_information.html', form=form, data=db_actions.get_all_cli_entries())
 
-@app.route('/message_board')
-def message_board():
-    return render_template('message_board.html')
+@app.route('/blog')
+def blog():
+    form = board_blog_forms.New_Blog_Post_Form()
+    if form.validate_on_submit():
+        # Adds Blog Post to DB
+        db_actions.create_blog_post(form.title.data, form.post.data)
+        # Gets a set of hashtags from back_end function and inserts them into the hashtag table
+        db_actions.add_new_tag(back_end.get_hashtags(form.post.data))
+    return render_template('blog.html', form=form, all_posts=db_actions.get_all_blog_posts())
 
 @app.route('/daily_log', methods=['GET', 'POST'])
 def daily_log():
@@ -43,6 +50,12 @@ def daily_log():
         db_actions.daily_post(form.post.data)
         return redirect(url_for('daily_log'))
     return render_template('daily_log.html', form=form, all_posts = db_actions.all_daily_posts())
+
+@app.rout('/message_board', methods=['GET', 'POST'])
+def message_board():
+    form = board_blog_forms.New_Message_Board_Post_Form()
+    if form.validate_on_submit():
+        db_actions.
 
 if __name__=='__main__':
     app.run(host=HOST, port=PORT, debug=DEBUG)
